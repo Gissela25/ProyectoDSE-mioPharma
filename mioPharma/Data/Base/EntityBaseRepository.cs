7284705d807705b9d958a05c999019f80f738a45
Jony-07 <jony25lopezml@gmail.com>
2022-10-04 23:30:29 -0600

    Base Repositorys

---
+++ mioPharma/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
+
+namespace mioPharma.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext _context;
+
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context= context;
+                
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            EntityEntry entityentry = _context.Entry<T>(entity);
+            entityentry.State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includePropieteis)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            query = includePropieteis.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+
+        public async Task UpdateAsync(int id, T entity)
+        {
+            EntityEntry entityentry = _context.Entry<T>(entity);
+            entityentry.State = EntityState.Modified;
+
+
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ mioPharma/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace mioPharma.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T:class, IEntityBase
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includePropieteis);
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+    }
+}
