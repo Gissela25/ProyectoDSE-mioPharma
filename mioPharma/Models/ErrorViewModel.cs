323a3610a63381e49ed2b420eb9327ce46678eaa
Gissela25 <gisselaverenice@gmail.com>
2022-10-04 21:14:27 -0600

    Agregar archivos de proyecto.

---
+++ mioPharma/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mioPharma.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
