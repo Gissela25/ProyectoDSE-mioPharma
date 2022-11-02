f94033c699db57188280c6088b61c118508b81d0
susan749 <60519933+susan749@users.noreply.github.com>
2022-11-01 23:15:44 -0600

    actualizacion imagenes

---
+++ mioPharma/Migrations/20221031043753_orders.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mioPharma.Migrations
+{
+    public partial class orders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
